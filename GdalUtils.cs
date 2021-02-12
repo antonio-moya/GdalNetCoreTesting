6a23b5f5f235295bcaecdd3871723aab083e5cff
Antonio Moya <antonio.moya@inclam.com>
2021-02-12 12:18:21 +0100

    Initial commit

---
+++ GdalUtils.cs
@@ -0,0 +1,365 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using NLog;
+using OSGeo.GDAL;
+using OSGeo.OGR;
+using OSGeo.OSR;
+
+namespace Test
+{
+    /// <summary>
+    /// Todo esto en: https://github.com/bertt/GdalOnNetCoreSample
+    /// </summary>
+    class GdalUtils
+    {
+
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+        
+        public static void GDalErrorHandler (int eclass, int code, IntPtr msg)
+        {
+            var message = msg != IntPtr.Zero ? Marshal.PtrToStringAnsi(msg) : string.Empty;
+            logger.Error($"==>=>=>=> Error GDAL: {message}");
+        }
+
+        public static int GDalProgress(double complete, System.IntPtr msg, System.IntPtr data) {
+            var message = msg != IntPtr.Zero ? Marshal.PtrToStringAnsi(msg) : string.Empty;
+            var datos = data != IntPtr.Zero ? Marshal.PtrToStringAnsi(data) : string.Empty;
+            logger.Info($"P:{(complete*100).ToString("0.0")}% {message} {data} ");
+            //logger.Trace($" {datos} ");
+            return 1;
+        }
+
+        /// <summary>
+        /// Configure and initialize GDAL libraries
+        /// </summary>
+        /// <param name="PathGdalBin">GDAL binaries</param>
+        /// <param name="PathGdalDriver">GDAL drivers/plugins binaries</param>
+        /// <param name="PathGdalData">EPSG CSV files, S-57 definition files, DXF header and footer files, ...</param>
+        public static void Configure(string PathGdalBin, string PathGdalDriver, string PathGdalData)
+        {
+            // Add GDal binaries to system PATH
+            if (Directory.Exists(PathGdalBin) && !string.IsNullOrEmpty(PathGdalBin)) {
+                string pathEnv = Environment.GetEnvironmentVariable("PATH");
+                if (!pathEnv.Contains(PathGdalBin))
+                {
+                    Environment.SetEnvironmentVariable("PATH", PathGdalBin + ";" + pathEnv);
+                }
+            }
+                
+            // Path to directory containing various GDAL data files
+            //   (EPSG CSV files, S-57 definition files, DXF header and footer files, ...).
+            // This option is read by the GDAL and OGR driver registration functions. 
+            // It is used to expand EPSG codes into their description in the OSR model (WKT based).
+            if (Directory.Exists(PathGdalData) && !string.IsNullOrEmpty(PathGdalData)) {
+                OSGeo.GDAL.Gdal.SetConfigOption("GDAL_DATA", PathGdalData);
+            }
+            
+            // Path to directory containing driver files that start with the prefix "gdal_X.dll".
+            if (Directory.Exists(PathGdalDriver) && !string.IsNullOrEmpty(PathGdalDriver)) {
+                OSGeo.GDAL.Gdal.SetConfigOption("GDAL_DRIVER_PATH", PathGdalDriver);
+            }
+            
+            // This line throws an exception if the the wrong version of GDal was found in the path somewhere,
+            // or the path didn't point to GDal correctly.
+            OSGeo.OGR.Ogr.RegisterAll();
+            OSGeo.GDAL.Gdal.AllRegister();
+
+        }
+
+        public static void GetGdalInfo()
+        {            
+            logger.Trace(Gdal.VersionInfo("RELEASE_NAME"));
+            logger.Trace(Gdal.VersionInfo("VERSION_NUM"));
+            logger.Trace(Gdal.VersionInfo("BUILD_INFO"));
+
+            var drivers = new List<string>();
+            for (var i = 0; i < Ogr.GetDriverCount(); i++)
+            {
+                drivers.Add(Ogr.GetDriver(i).GetName());
+            }
+            logger.Trace(string.Join(",", drivers));
+            
+            drivers = new List<string>();
+            for (var i = 0; i < Gdal.GetDriverCount(); i++)
+            {
+                drivers.Add(Gdal.GetDriver(i).GetDescription());
+            }
+            logger.Trace(string.Join(",", drivers));
+        }
+
+        public static void ReprojectCoordinatesExample() {
+
+            var src = new OSGeo.OSR.SpatialReference("");
+            src.ImportFromEPSG(23030);
+            Console.WriteLine($"SOURCE IsGeographic:" + src.IsGeographic() + " IsProjected:" + src.IsProjected());
+            var dst = new OSGeo.OSR.SpatialReference("");
+            dst.ImportFromEPSG(4326);
+            Console.WriteLine("DEST IsGeographic:" + dst.IsGeographic() + " IsProjected:" + dst.IsProjected());
+            var ct = new OSGeo.OSR.CoordinateTransformation(src, dst);
+            double[] p = new double[3];
+            p[0] = 85530; p[1] = 446100; p[2] = 0;
+            Console.WriteLine("From: x:" + p[0] + " y:" + p[1] + " z:" + p[2]);
+            ct.TransformPoint(p);
+            Console.WriteLine("To: x:" + p[0] + " y:" + p[1] + " z:" + p[2]);
+        }
+
+        /// <summary>
+        /// Translation of raster format
+        /// </summary>
+        /// <param name="InputRaster"></param>
+        /// <param name="OutputRaster"></param>
+        /// <param name="OutputDriver">driver neme for the output file, from GDAL drivers list</param>
+        public static void TranslateRasterFormat(string InputRaster, string OutputRaster, string OutputDriver) {
+            try
+            {
+                var opts = new string[] { "-sds", "-of", OutputDriver };
+                using(var ds = Gdal.Open( InputRaster, Access.GA_ReadOnly )) {
+                    var options = new GDALTranslateOptions(opts);
+                    using(var outputDS = Gdal.wrapper_GDALTranslate(OutputRaster, ds, options, (Gdal.GDALProgressFuncDelegate)GDalProgress, $"Translare {InputRaster} to {OutputRaster} ({OutputDriver})" )) {
+                        outputDS.FlushCache();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Application error: " + e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Reproject a raster
+        /// </summary>
+        /// <param name="InputFilePath"></param>
+        /// <param name="OutputFilePath"></param>
+        /// <param name="EPSG">output EPSG</param>
+        public static void RasterReprojection(string InputFilePath, string OutputFilePath, int EPSG) 
+        {
+            var sr = new OSGeo.OSR.SpatialReference(null);
+            sr.ImportFromEPSG(EPSG);
+            sr.ExportToWkt(out var srs_wkt);
+
+            // reproject raster and save to EHdr
+            using( var srcDs = Gdal.Open(InputFilePath, Access.GA_ReadOnly))
+            using( var vrt = Gdal.AutoCreateWarpedVRT(srcDs, srcDs.GetProjectionRef(), srs_wkt, ResampleAlg.GRA_Average, 0)) {
+                var OutDirver = srcDs.GetDriver();
+                OutDirver.CreateCopy(OutputFilePath, vrt, 1, null, GDalProgress, "Raster Reprojection");
+            }
+            //using( var MemDirver = Gdal.GetDriverByName("MEM"))
+
+            //using( var bilDirver = srcDs.GetDriver() Gdal.GetDriverByName("EHdr"))
+            //using( var bilfile = bilDirver.CreateCopy("lo que sea/asd/", vrt, 1, null,null, null))
+     
+            return;
+        }
+
+        /// <summary>
+        /// Build a contour vector layer
+        /// </summary>
+        /// <param name="InputRaster"></param>
+        /// <param name="OutputShp">output shapefile</param>
+        /// <param name="ContoutInterval">increment balue (from CotourBase) to create contour</param>
+        /// <param name="ContourBase">initial value to compute contour</param>
+        public static void Contour(string InputRaster, string OutputShp, double ContourInterval, double ContourBase) {
+
+            // https://gis.stackexchange.com/questions/210500/calling-gdal-contour-from-python-ipython
+
+            using(var dsc = Gdal.Open(InputRaster, Access.GA_ReadOnly)) {
+
+                var band = dsc.GetRasterBand(1);
+                
+                // Generate layer to save Contourlines in
+                using(var ogr_ds = Ogr.GetDriverByName("ESRI Shapefile").CreateDataSource(OutputShp,  new string[] { "srs", dsc.GetProjection() })) {
+
+                    var srs = new SpatialReference(dsc.GetProjection());
+                    var contour_shp = ogr_ds.CreateLayer("contour", srs, wkbGeometryType.wkbLineString, null);
+                    contour_shp.CreateField(new FieldDefn("ID", FieldType.OFTInteger), 1);
+                    contour_shp.CreateField(new FieldDefn("ELEVATION", FieldType.OFTReal), 2);
+
+                    //Generate Contour lines
+                    Gdal.ContourGenerate(band, ContourInterval, ContourBase, 0, null, 0, -9999.9, contour_shp, contour_shp.FindFieldIndex("ID",0), contour_shp.FindFieldIndex("ELEVATION",0), GDalProgress, string.Empty);
+                }
+            }
+
+        }
+
+
+        /// <summary>
+        /// Creates a GeoTIFF file
+        /// </summary>
+        /// <param name="InputRaster"></param>
+        /// <param name="Rows"></param>
+        /// <param name="Columns"></param>
+        /// <param name="EPSG"></param>
+        /// <param name="RasterMetadata"></param>
+        /// <param name="BandMetadata"></param>
+        public static void CreateRaster(string InputRaster, int Rows, int Columns, double MinX, double MinY, double CellSize, int EPSG, IEnumerable<float[]> BandsValues, string RasterMetadata, string BandMetadata) {
+
+            var NumBands = 1;
+            if (BandsValues != null) NumBands = BandsValues.Count();
+            
+            int height = Rows;
+            int width = Columns;
+            int bXSize = width;
+            int bYSize = 1;
+
+            try
+            {
+                /* -------------------------------------------------------------------- */
+                /*      Get drivers                                                      */
+                /* -------------------------------------------------------------------- */
+                var GeotiffDriver = Gdal.GetDriverByName("GTiff");
+
+                /* -------------------------------------------------------------------- */
+                /*      Create geotiff dataset.                                         */
+                /* -------------------------------------------------------------------- */
+                string[] options = new string [] { $"BLOCKXSIZE={bXSize}", $"BLOCKYSIZE={bYSize}" };
+                using(var ds = GeotiffDriver.Create(InputRaster, width, height, NumBands, DataType.GDT_Float32, options)) {
+                    
+                    // Set geo transform
+                    // https://stackoverflow.com/questions/27166739/description-of-parameters-of-gdal-setgeotransform
+                    var arg = new[] { MinX, CellSize, 0, MinY, 0, CellSize };
+                    ds.SetGeoTransform(arg);
+
+                    // Set coordinate system    
+                    var src = new OSGeo.OSR.SpatialReference("");
+                    src.ImportFromEPSG(EPSG);
+                    src.ExportToWkt(out var proj_wkt);
+                    ds.SetProjection(proj_wkt);
+
+                    // Set metadata
+                    ds.SetMetadataItem("SUAT", RasterMetadata, string.Empty);
+                    
+                    /* -------------------------------------------------------------------- */
+                    /*      Setting corner GCPs.                                            */
+                    /* -------------------------------------------------------------------- */
+                    /*
+                        GCP[] GCPs = new GCP[] {
+                        new GCP(44.5, 27.5, 0, 0, 0, "info0", "id0"),
+                        new GCP(45.5, 27.5, 0, 100, 0, "info1", "id1"),
+                        new GCP(44.5, 26.5, 0, 0, 100, "info2", "id2"),
+                        new GCP(45.5, 26.5, 0, 100, 100, "info3", "id3")
+                    };
+                    ds.SetGCPs(GCPs, ""); */
+
+                    // Escribir bandas
+                    int NumBand = 1;
+                    foreach(var vals in BandsValues) {
+                        var BandValues = vals;
+                        if (vals.Length < (width*height)) {
+                            logger.Error(new ArgumentOutOfRangeException("BandsValues",vals, $"La longitud de los datos de la banda {NumBand} es menor que el número de filas por columnas."));
+                            BandValues = new float[width*height];
+                        }
+                        var band = ds.GetRasterBand(NumBand);
+                        band.SetNoDataValue(-9999.0);
+                        band.WriteRaster(0, 0, width, height, BandValues, width, height, 0, 0);
+                        band.FlushCache();
+                        NumBand++;
+                    }
+                    
+                    // Persistir a disco
+                    ds.FlushCache();
+                }
+
+                /* -------------------------------------------------------------------- */
+                /*      Create MEM dataset to support AddBand not during creation.      */
+                /* -------------------------------------------------------------------- */
+                /*
+                var MemDriver = Gdal.GetDriverByName("MEM");
+                OSGeo.GDAL.Dataset dsMem = null;
+                using(var dsGeoTiff = Gdal.Open(InputRaster, Access.GA_ReadOnly)) {
+                    dsMem = Gdal.GetDriverByName("MEM").CreateCopy("", dsGeoTiff, 0, null, (Gdal.GDALProgressFuncDelegate)GDalProgress, "GTiff to MEM copy" );
+                }
+                for(int band=1; band <5; band++) {
+
+                    if (band>1) dsMem.AddBand(DataType.GDT_CFloat32, null);
+
+                    var ba = dsMem.GetRasterBand(band);
+                    var buffer = new float [w * h];
+                    for (int i = 0; i < w; i++)
+                        for (int j = 0; j < h; j++)
+                            buffer[i * w + j] = (float)(i * 256 / w) * band;
+                    ba.WriteRaster(0, 0, w, h, buffer, w, h, 0, 0);
+                    ba.FlushCache();
+                }
+                dsMem.FlushCache();
+                
+                // Copy to source GeoTiff
+                File.Delete(InputRaster);
+                var dsTiff = GeotiffDriver.CreateCopy(InputRaster, dsMem, 0, null, (Gdal.GDALProgressFuncDelegate)GDalProgress, "MEM to Gtiff copy" );
+
+                dsTiff.Dispose();
+                dsMem.Dispose();
+                */
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Application error: {e.Message} {Gdal.GetLastErrorMsg()}");
+            }
+
+        }
+
+        /// <summary>
+        /// Performs IDW with NN surface interpolation
+        /// </summary>
+        /// <param name="InputVector"></param>
+        /// <param name="OutputTIFF"></param>
+        public static void IdwInterpolation(string InputVector, string OutputTIFF) {
+
+            // No se realiza ningún tipo de reproyección, el TIFF se genera con el mismo sistema de coordenadas que el vectorial de entrada
+
+            // Dimensiones del raster de salida (todas las coordenadas se establecel en unidades del sistema de coordinadas del vectorial de entrada)
+            double CellSize = 10000;
+            double xMin = 360000;
+            double yMax = 4830000;
+            int NumCols = 59;
+            int NumRows = 39;
+            double yMin = yMax - (NumRows*CellSize);
+            double xMax = xMin + (NumCols*CellSize);
+
+            //-----------------------------
+            // Parámetros de interpolación
+            //-----------------------------
+            var cul = System.Globalization.CultureInfo.InvariantCulture;
+            var parameters = new List<string>();
+            parameters.AddRange(new string[] {"-zfield", "rainfall"}); // Campo con datos para interpolar
+            parameters.AddRange(new string[] {"-txe", xMin.ToString(cul),xMax.ToString(cul)});
+            parameters.AddRange(new string[] {"-tye", yMin.ToString(cul),yMax.ToString(cul)});
+            parameters.AddRange(new string[] {"-outsize", NumCols.ToString(cul),NumRows.ToString(cul)});
+            // algoritmo a utilizar (https://gdal.org/programs/gdal_grid.html#interpolation-algorithms)
+            double radious = Math.Max((xMax-xMin)/2, (yMax-yMin)/2);
+            parameters.AddRange( new string[] {"-a", $"invdistnn:power=2.0:smothing=0.0:radius={radious.ToString(cul)}:max_points=12:min_points=5:nodata=0.0"});
+            parameters.AddRange(new string[] {"-of", "gtiff"}); // formato de salida
+            parameters.AddRange(new string[] {"-ot", "Float32"}); // tipo de datos de salida
+
+            Console.WriteLine("Parámetros: " + string.Join(" ", parameters));
+
+            //-----------------------------
+            // Vectorial de entrada
+            // Si el vectorial tiene algún valor no válido deben ser limpiados aquí
+            // Ejecución del algoritmo
+            //-----------------------------
+            using(var ds = Gdal.OpenEx(InputVector, 0, null, null, null)) {
+                
+                var gridDS =  Gdal.wrapper_GDALGrid(OutputTIFF, ds, new GDALGridOptions(parameters.ToArray()), (Gdal.GDALProgressFuncDelegate) GDalProgress, string.Empty);
+                gridDS.SetDescription("SUAT.IDW from pluviometers");
+                //gridDS.SetMetadata( {"": '1', 'key2': 'yada'} );
+            }
+        }
+
+
+        /* 
+        Codificación de shapefiles (probablemente importante cuando hay que crearlos:)
+        https://www.programmersought.com/article/9249124261/
+        OSGeo.GDAL.Gdal.SetConfigOption("GDAL_FILENAME_IS_UTF8", "NO");
+        // In order for the property sheet field to support Chinese, please add the following sentence
+        OSGeo.GDAL.Gdal.SetConfigOption("SHAPE_ENCODING", ""); */
+        /*
+        GeoJson a Shapefile y escritura lectura de GeoTiffs en C#: https://github.com/adadevelopment/adadevfiles/tree/master/files
+        GDal rasterize en C#: http://osgeo-org.1560.x6.nabble.com/gdal-dev-GDAL-C-bindings-read-from-vsimem-td5371149.html
+        GDAl read and write TIff files C#: https://adadevelopment.github.io/gdal/gdal-read-write.html
+        */
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using Microsoft.Extensions.Configuration;
+using NLog;
+using OSGeo.GDAL;
+using OSGeo.OGR;
+using OSGeo.OSR;
+
+namespace Test
+{
+    /// <summary>
+    /// Todo esto en: https://github.com/bertt/GdalOnNetCoreSample
+    /// </summary>
+    class Program
+    {
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+        
+        static void Main(string[] args)
+        {
+
+            /* -------------------------------------------------------------------- */
+            /*      Read config file  .                                             */
+            /* -------------------------------------------------------------------- */
+            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile($"appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{env}.json", true, true)
+                .AddEnvironmentVariables();
+            var config = builder.Build();
+
+            /* -------------------------------------------------------------------- */
+            /*      Register driver(s).                                             */
+            /* -------------------------------------------------------------------- */
+            try
+            {            
+                Gdal.PushErrorHandler (new Gdal.GDALErrorHandlerDelegate (GdalUtils.GDalErrorHandler));
+                GdalUtils.Configure(config["GDAL:PATH_GDAL_BIN"], config["GDAL:PATH_GDAL_DRIVER"], config["GDAL:PATH_GDAL_DATA"]);
+                Gdal.UseExceptions();   
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, ex.StackTrace + " " + Gdal.GetLastErrorMsg());
+            }
+
+            AKIII: leer congfigs json en función del SO
+            var datapath = config["DATA_PATH"];
+            //if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) datapath = "/home/local/NILO/antonio/Escritorio/GeoTiffTests/data/";
+            //if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) datapath = @"C:\XXX\GeoTiffTests\data\";
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX)) { Console.WriteLine("MAC not supported"); System.Environment.Exit(0); }
+
+            if (true) {
+                Console.WriteLine("===================================================================");
+                Console.WriteLine("===========================REPROJECTION coordinate testing==================================");
+                try
+                {
+                    GdalUtils.ReprojectCoordinatesExample();
+                }
+                catch (Exception ex)
+                {
+                    logger.Error(ex, ex.StackTrace + " " +  Gdal.GetLastErrorMsg());
+                }
+            }
+
+           if (false) {
+                Console.WriteLine("===================================================================");
+                Console.WriteLine("===========================GDAL INFO==================================");
+                try
+                {
+                    GdalUtils.GetGdalInfo();
+                }
+                catch (Exception ex)
+                {
+                    logger.Error(ex, Gdal.GetLastErrorMsg());
+                }
+            }
+
+            if (true) {
+                Console.WriteLine("===================================================================");
+                Console.WriteLine("========================Crear GTiff y añadir bandas =====================================");
+                try
+                {
+                    var output = Path.Combine(datapath, "CreateRasterNew.tiff");
+                    if (File.Exists(output)) File.Delete(output);
+
+                    int NRows = 200;
+                    int Ncols = 100;
+                    double MinX = 55;
+                    double MinY = 45;
+                    double CellSize = 0.1;
+                    int EPSG = 4326;
+
+                    var valores = new List<float[]>();
+                    for(var band=0; band<5;band++) {
+                        var buffer = new float [NRows * Ncols];
+                        for (int i = 0; i < Ncols; i++)
+                            for (int j = 0; j < NRows; j++)
+                                buffer[i * Ncols + j] = (float)(i * 256 / Ncols) * band;
+                        valores.Add(buffer);
+                    }
+                    GdalUtils.CreateRaster(output, NRows,Ncols,MinX, MinY,CellSize, EPSG, valores, null, null);
+                }
+                catch (System.Exception ex)
+                {
+                    logger.Error(ex, Gdal.GetLastErrorMsg());
+                }
+            }
+
+            if (true) {
+                Console.WriteLine("===================================================================");
+                Console.WriteLine("========================Raster reprojection =====================================");
+                try
+                {
+                    int OutEpsg = 23030;
+                    var input = Path.Combine(datapath,"CreateRasterNew.tiff");
+                    var output = Path.Combine(datapath, $"CreateRasterNew{OutEpsg}.tiff");
+                    if (File.Exists(output)) File.Delete(output);
+                    if (File.Exists(output)) File.Delete(output);
+                    GdalUtils.RasterReprojection(input, output, OutEpsg);
+                }
+                catch (System.Exception ex)
+                {
+                    logger.Error(ex, Gdal.GetLastErrorMsg());
+                }
+            }
+
+            if (false) {
+                Console.WriteLine("===================================================================");
+                Console.WriteLine("========================Translate GRIB2 => GTiff =====================================");
+                try
+                {
+                    var input = "";
+                    var output = "";
+                    /*
+                    // Funciona pero tarda mucho:
+                    input = Path.Combine(datapath,"CHEBROe00.20201125.grib2");;
+                    output = Path.Combine(datapath, "CHEBROe00.20201125.tiff");
+                    if (File.Exists(output)) File.Delete(output);
+                    GDALSpatialInterpolation.TranslateRasterFormat(input, output, "GTiff");
+                    */
+                    input = Path.Combine(datapath,"pluviometrosIDW.tiff");
+                    output = Path.Combine(datapath, "pluviometrosIDW.asc");
+                    if (File.Exists(output)) File.Delete(output);
+                    GdalUtils.TranslateRasterFormat(input, output, "AAIGrid");
+                }
+                catch (System.Exception ex)
+                {
+                    logger.Error(ex, Gdal.GetLastErrorMsg());
+                }
+            }
+
+            if (false) {
+                Console.WriteLine("===================================================================");
+                Console.WriteLine("========================IDW=====================================");
+                try
+                {
+                    GdalUtils.IdwInterpolation(Path.Combine(datapath, "pluviometros_23030.shp"),Path.Combine(datapath, "pluviometrosIDW.tiff"));
+                }
+                catch (System.Exception ex)
+                {
+                    logger.Error(ex, Gdal.GetLastErrorMsg());
+                }
+            }
+
+            if (false) {
+                Console.WriteLine("===================================================================");
+                Console.WriteLine("========================Contour=====================================");
+                try
+                {
+                    var input = Path.Combine(datapath, "pluviometrosIDW.tiff");
+                    var output = Path.Combine(datapath, "contour.shp");
+                    if (File.Exists(input)) File.Delete(input);
+                    if (File.Exists(output)) File.Delete(output);
+                    GdalUtils.Contour(input, output, 1d, 0d);
+                }
+                catch (System.Exception ex)
+                {
+                    logger.Error(ex, Gdal.GetLastErrorMsg());
+                }
+            }
+
+            if (false) {
+                Console.WriteLine("===================================================================");
+                Console.WriteLine("=============================Info GEOTIFF Multiband======================================");
+                GDALInfo.Info(Path.Combine(datapath,"CHEBROe00.20201125.tif"), false);
+            }
+            if (false) {
+                Console.WriteLine("===================================================================");
+                Console.WriteLine("==============================Info GRIB2=====================================");
+                GDALInfo.Info(Path.Combine(datapath,"CHEBROe00.20201125.grib2"), true);   
+            }
+            if (false) {
+                //SepararEnBandas(Path.Combine(datapath,"CHEBROe00.20201125.grib2"));    
+            }
+
+        }
+    }
+}
